61457cfdede8deb09135747083dcb3a24bb482d0
gzMichael <1960809@qq.com>
2017-03-19 02:19:08 +0800

    update backtest.py

--- app/backtest.py
+++ app/backtest.py
@@ -15,7 +15,7 @@ def backtest(conn, stockcode, start, end, period, fund):
         String: The date of start in format("%Y-%m-%d").
         String: The date of end in format("%Y-%m-%d").
         String: The period of the backtest:Monthly or Yearly.
-        String: The amount of fund of investment.
+        Int: The amount of fund of investment.
     Return:
         List: btrecords: [list_date, list_datetime, list_asset, list_cash, 
                             list_holding, list_price]
@@ -44,13 +44,14 @@ def backtest(conn, stockcode, start, end, period, fund):
     #输入的初始日期和结束日期正常
     if dt_start <= dt_end:
         dt = dt_start
-        sql_query = ("SELECT date,close FROM %s WHERE date>='%s' AND date<='%s'"
+        sql_query = ("SELECT date,close FROM %s WHERE date>='%s' AND date<='%s' "
                     "ORDER BY date"%(table_name,start,end)
                     )
         index = 0
         result = cur.execute(sql_query).fetchall()
+        print('sql=%s, result=%s, len(result)=%s'%(sql_query, result, len(result)))
         #查询到历史数据
-        if result is not None:
+        if len(result) > 0:
             #开始回测过程
             while dt <= dt_end:
                 #提取当天日期
@@ -92,11 +93,11 @@ def backtest(conn, stockcode, start, end, period, fund):
                     list_asset.append(asset)
                     list_date.append(dt.strftime("%Y-%m-%d"))
                     list_datetime.append(time.mktime(dt.timetuple()))
-                    print('%s 账户有操作！：asset=%s, cash=%s, holding=%s, price=%s'%(dt.strftime("%Y-%m-%d"),asset,cash,holding,close_record))
+                    #print('%s 账户有操作！：asset=%s, cash=%s, holding=%s, price=%s'%(dt.strftime("%Y-%m-%d"),asset,cash,holding,close_record))
                 #模拟往后一天
                 dt = dt + datetime.timedelta(days=1)
                 needs_recorded = False
-        #该品种没有查询到历史行情
+        #该品种没有查询到历史行情，只能定期存入现金
         else:
             while dt <= dt_end:
                 day = dt.day
@@ -110,6 +111,7 @@ def backtest(conn, stockcode, start, end, period, fund):
                         list_price.append(0)
                         list_asset.append(asset)
                         list_date.append(dt.strftime("%Y-%m-%d"))
+                        list_datetime.append(time.mktime(dt.timetuple()))
                     else:
                         if month == month_invest:
                             cash = cash + fund    
@@ -119,12 +121,78 @@ def backtest(conn, stockcode, start, end, period, fund):
                             list_price.append(0)
                             list_asset.append(asset)
                             list_date.append(dt.strftime("%Y-%m-%d"))
+                            list_datetime.append(time.mktime(dt.timetuple()))
                 dt = dt + datetime.timedelta(days=1)
         btrecords = [list_date, list_datetime, list_asset, list_cash, list_holding, list_price]
-        return btrecords, 0    
-    #初始日期大于结束日期        
+        error_str = ''
+        rscode = 0
+        return btrecords, error_str, rscode
+    #初始日期大于结束日期
     else:
-        return [], 1
+        btrecords = []
+        error_str = '初始日期大于结束日期'
+        rscode = 1
+        return btrecords, error_str, rscode
+    
+def backtest_chart(btrecords, stockcode):
+    import os
+    import matplotlib.pyplot as plt
+    from matplotlib.dates import AutoDateLocator, DateFormatter
+    from matplotlib.font_manager import FontProperties
+    try:
+        list_date, list_datetime, list_asset, list_cash, list_holding, list_price = btrecords
+    except:
+        images = []
+        error_str = '不能读取回测结果'
+        rscode = 1
+        return images, rscode, error_str
+    else:    
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        imagefile_dir = os.path.join(basedir, 'static\\')
+        w = []
+        x = []
+        y = []
+        z = []
+        #X轴数据过多，需要合并来缩短显示数量
+        if len(list_datetime) > 30:
+            t = len(list_datetime) // 30
+            for i in range(0,len(list_datetime)-1):
+                if i % t == 0 :
+                    w.append(list_holding[i])
+                    x.append(list_datetime[i])
+                    y.append(list_asset[i])
+                    z.append(list_date[i])
+        else:
+            w = list_holding
+            x = list_datetime
+            y = list_asset
+            z = list_date
+        print('len(w)=%s, len(x)=%s, len(y)=%s, len(z)=%s'%(len(w),len(x),len(y),len(z)))
+        font = FontProperties(fname = "c:/windows/fonts/simsun.ttc", size=14) 
+        plt.figure(figsize=(8, 6))
+        plt.plot(x,y)
+        plt.xticks(x,z,rotation=55)
+        plt.xlabel(u'年月',fontproperties=font)
+        plt.ylabel(u'账户资产',fontproperties=font)
+        plt.title(u'定投 %s 收益情况'%stockcode,fontproperties=font)
+        imagefile_urls = []
+        imagefile_url = imagefile_dir + 'a.png'
+        imagefile_urls.append(imagefile_url)
+        plt.savefig(imagefile_url)
+        #plt.show()
+        plt.figure(figsize=(8, 6))
+        plt.bar(left=x,height=w,width=1000000,align='center')
+        plt.xticks(x,z,rotation=55)
+        plt.xlabel(u'年月',fontproperties=font)
+        plt.ylabel(u'持仓数量',fontproperties=font)
+        plt.title(u'持仓标的 %s 的情况'%stockcode,fontproperties=font)
+        imagefile_url = imagefile_dir + 'b.png'
+        imagefile_urls.append(imagefile_url)
+        plt.savefig(imagefile_url)
+        #plt.show()
+        error_str = ''
+        rscode = 0
+        return imagefile_urls, error_str, rscode
     
 if __name__ == '__main__':
     import os
@@ -136,38 +204,21 @@ if __name__ == '__main__':
     try:
         conn = sqlite3.connect(SQLITE_DATABASE_URI)
         cur = conn.cursor()
-        table_name = 'stock_000563'
-        start = '2015-12-20'
-        end = '2017-01-20'
+        start = '2000-12-01'
+        end = '2003-02-2'
         fund = 5000
         period = 'monthly'
         stockcode = '000002'
-        btrecords,code = backtest(conn,stockcode,start,end,period,fund)
-        list_date, list_datetime, list_asset, list_cash, list_holding, list_price = btrecords
-        x=list_datetime
-        y=list_asset
-        z=list_date
-        #print('x=%s, type(x)=%s'%(x,type(x)))
-        #print('y=%s, type(x)=%s'%(y,type(y)))
-        font = FontProperties(fname = "c:/windows/fonts/simsun.ttc", size=14) 
-        plt.figure(figsize=(10, 6))
-        plt.plot(x,y)
-        plt.xticks(x,z,rotation=27)
-        plt.xlabel(u'年月',fontproperties=font)
-        plt.ylabel(u'账户资产',fontproperties=font)
-        plt.title(u'定投 %s 收益情况'%stockcode,fontproperties=font)
-        plt.savefig('d:/a.png')
-        plt.show()
-        y=list_holding
-        plt.figure(figsize=(10, 6))
-        plt.bar(left=x,height=y,width=1000000,align='center')
-        plt.xticks(x,z,rotation=27)
-        plt.xlabel(u'年月',fontproperties=font)
-        plt.ylabel(u'持仓数量',fontproperties=font)
-        plt.title(u'持仓标的 %s 的情况'%stockcode,fontproperties=font)
-        plt.savefig('d:/b.png')
-        plt.show()
+        table_name = 'stock_%s'%stockcode
+        btrecords, error_str, rscode = backtest(conn,stockcode,start,end,period,fund)
+        if rscode == 0:
+            images, error_str, ret = backtest_chart(btrecords, stockcode)
+            if ret != 0:
+                print(error_str)
+        else:
+            print(error_str)
     finally:
-        if conn:
+        if cur:
             cur.close()
+        if conn:            
             conn.close()
