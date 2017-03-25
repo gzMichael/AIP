d65f430f03a55aba0e925afc6d76cd49d65ecef2
gzMichael <1960809@qq.com>
2017-03-26 01:14:27 +0800

    update collectdata.py with fund data

--- app/collectdata.py
+++ app/collectdata.py
@@ -1,38 +1,67 @@
 # -*- coding: utf-8 -*-
 
+'''
+功能：更新所有基金和股票的历史记录。
+说明：本功能模块**单独运行**，没有其他模块调用。需设置在系统的定期运行任务中。
+版本：V1.0
+作者：gzMichael
+'''
+    
 import os
 import tushare as ts
 import sqlite3
 import datetime
 import re,requests
 from bs4 import BeautifulSoup
+import pandas as pd
     
-def update_stock_data(conn):
+def update_stock_data():
     '''Update all stock history records to the present day.
+       Stock list table: 'stock_basics'
+       Single stock history table: 'stock_xxxxxx'
     Args: 
-        Connection of database
+        None
     Return: 
         None
     '''
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLITE_DATABASE_URI = os.path.join(basedir, '../stock.sqlite')
+    conn = sqlite3.connect(SQLITE_DATABASE_URI)
     df = ts.get_stock_basics()
     df.to_sql('stock_basics', conn, flavor='sqlite', if_exists='replace')
+    if conn:
+        conn.close()
     dtnow = datetime.datetime.now()
     dtstr = dtnow.strftime('%Y-%m-%d')
-    print(dtstr)
     rowcount = 1
     for i in df.index:
         #查询表名
         table_name = 'stock_%s'%i
         sql_str = "SELECT name FROM sqlite_master WHERE type='table' AND name='%s'"%table_name
-        rows = cur.execute(sql_str).fetchall()
-        progress_str = '%s/%s'%(rowcount,len(df))
+        conn = sqlite3.connect(SQLITE_DATABASE_URI)
+        cur = conn.cursor()
+        try:
+            rows = cur.execute(sql_str).fetchall()
+        except sqlite3.OperationalError as error:
+            print('操作Sqlite3数据库出现错误，错误信息是：%s'%error)
+        finally:
+            if cur:
+                cur.close()
+            if conn:
+                conn.close()
+        progress_str = '股票更新总进度：%s/%s'%(rowcount,len(df))
         #如果表已经存在，插入最新数据
         if len(rows) > 0:
             for row in rows:
                 print('%s:表%s已存在，正在检查更新...'%(progress_str, table_name))
                 sql_query_date = "SELECT date FROM %s ORDER BY date DESC LIMIT 1"%table_name
+                conn = sqlite3.connect(SQLITE_DATABASE_URI)
+                cur = conn.cursor()
                 try:
                     rs = conn.execute(sql_query_date).fetchone()
+                except:
+                    print('查询数据库出错，无法查询表%s内容。'%table_name)
+                else:    
                     if rs is None:
                         cur.execute('DROP TABLE IF EXISTS %s'%table_name)
                         #表格没有数据，从最早日期开始查询
@@ -45,11 +74,9 @@ def update_stock_data(conn):
                     #补充最后一条数据到今天的记录
                     df2 = ts.get_k_data(i, start=dt1_str, end=dtstr)
                     if len(df2) > 0:
-                        #print(df2)
-                        
                         for i in range(0,len(df2)):
                             object = df2.values[i]
-                            print('object=%s'%object)
+                            #print('object=%s'%object)
                             #df2.to_sql(table_name, conn, flavor='sqlite', if_exists='replace')
                             value_str = ''
                             for i in range(0,len(object)):
@@ -57,14 +84,13 @@ def update_stock_data(conn):
                                 if i < len(object)-1:
                                     value_str = value_str + ', '
                         sql_insert = "INSERT INTO %s(date,open,close,high,low,volume,code) values (%s)"%(table_name,value_str)
-                        #print('sql_insert=%s, value_str= %s'%(sql_insert,value_str))
                         try:
                             conn.execute(sql_insert)
                             conn.commit()
                             print('更新了从%s 到%s 的日线记录：共%s条记录已更新.'%(dt1_str,dtstr,len(df2)))
-                        except:    
+                        except sqlite3.OperationalError as error:
+                            print('更新数据库错误：无法插入数据，回滚该次数据库操作。错误信息是：%s'%error)
                             conn.rollback()
-                            print('更新数据库错误：无法插入数据，回滚该次数据库操作。')
                     else:
                         df3 = ts.get_k_data(i, start='1990-01-01', end=dtstr)
                         sql2 = "SELECT date FROM %s"%table_name
@@ -74,94 +100,155 @@ def update_stock_data(conn):
                         else:
                             print('表格%s的日线有缺失，需要重新下载%s条数据。'%(i, len(df3)))
                             df3.to_sql(table_name, conn, flavor='sqlite', if_exists='replace')
-                except:
-                    print('查询数据库出错，无法查询表%s内容。'%table_name)
+                finally:
+                    if cur:
+                        cur.close()
+                    if conn:
+                        conn.close()
         else:
             print('%s:表%s不存在，现在下载%s的全部日线数据...'%(progress_str, table_name, i))
             df2 = ts.get_k_data(i,start='1990-01-01',end=dtstr)
-            df2.to_sql(table_name, conn, index=True)
-            print('%s条数据已更新。'%len(df2))
+            conn = sqlite3.connect(SQLITE_DATABASE_URI)
+            try:
+                df2.to_sql(table_name, conn, index=True)
+            except:
+                    print('查询数据库出错，无法查询表%s内容。'%table_name)
+            else:
+                print('%s条数据已更新。'%len(df2))
+            finally:
+                if conn:
+                    conn.close()
         rowcount = rowcount + 1
     return
     
-def update_fund_data(conn):
-    url = "http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code-%s&page=1&per=9999"%fund_code
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.content,'lxml')
-    creatime = ""
-    data = ""
-    dict_funds ={}
-    table = soup.find("table", {"class" : "w782 comm lsjz"})
-    td_th = re.compile('t[dh]')
-
-    for row in table.findAll("td"):
-        cells = row.findAll(td_th)
-        if len(cells) == 7:
-            creatime = cells[0].find(text=True)
-            if not creatime:
-                continue
-            creatime = cells[0].find(text=True)
-            data = cells[2].find(text=True)
-            dict_funds[creatime] = data
-
+def update_fund_data():
+    '''Update all fund history records to the present day.
+       Fund list table: 'stock_basics'
+       Single fund history table: 'fund_xxxxxx'
+    Args: 
+        None
+    Return: 
+        None
+    '''
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLITE_DATABASE_URI = os.path.join(basedir, '../stock.sqlite')
+    conn = sqlite3.connect(SQLITE_DATABASE_URI)
+    cur = conn.cursor()
+    sql_query_fundcode = "SELECT code FROM fund_basics"
+    try:
+        rows = cur.execute(sql_query_fundcode).fetchall()
+        #print(rows)
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+    rowcount = 1
+    for row in rows:
+        progress_str = '基金更新总进度：%s/%s'%(rowcount,len(rows))
+        print(progress_str)
+        fund_code = row
+        table_name = 'fund_%s'%fund_code
+        url = "http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&page=1&per=9999"%fund_code
+        print('code=%s, type(code)=%s, url=%s'%(fund_code,type(fund_code),url))
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.content,'lxml')
+        creatime = ""
+        data = ""
+        list_date =[]
+        list_netvalue = []
+        list_cumnetvalue = []
+        list_dailygrowth = []
+        table = soup.find("table", {"class" : "w782 comm lsjz"})
+        td_th = re.compile('t[dh]')
+        for row in table.findAll("tr"):
+            cells = row.findAll(td_th)
+            if len(cells) == 7 and cells[0].find(text=True) != '净值日期':
+                list_date.append(cells[0].find(text=True))
+                list_netvalue.append(cells[1].find(text=True))
+                list_cumnetvalue.append(cells[2].find(text=True))
+                list_dailygrowth.append(cells[3].find(text=True))
+        df = pd.DataFrame({ 'date' : list_date,
+                            'netvalue' : list_netvalue,
+                            'cum_netvalue' : list_cumnetvalue,
+                            'dailygrowth' : list_dailygrowth })
+        rowcount = rowcount + 1
+        conn = sqlite3.connect(SQLITE_DATABASE_URI)
+        try:
+            df.to_sql(table_name, conn, flavor='sqlite', if_exists='replace')
+        except sqlite3.OperationalError as error:
+            print('操作Sqlite3数据库出现错误，错误信息是：%s'%error)
+        else:
+            print('已完成表格 %s 的更新，共 %s 条数据。'%(table_name,len(df)))
+        finally:
+            if conn:
+                conn.close()
+        #print(df)
+    print('全部基金数据已经完成更新，共更新 %s 个表格。'%len(rows))
+        
 def get_fund_lists():
+    '''Get all fund list from local file, and create/update the 'fund_basics' table.
+    Args: 
+        Connection of database
+    Return: 
+        None
+    '''
     # url = 'http://fund.eastmoney.com/fund.html#os_0;isall_1;ft_|;pt_1'
     # resp = requests.get(url)
     # soup = BeautifulSoup(resp.content,'lxml')
-    html = open("all_funds_p1.html", "r")
+    html = open("all_funds.html", "r")
     table = BeautifulSoup(html,'lxml')
     rows = table.find_all('tr')
-    list_fund_id = []
-    list_fund_code = []
-    list_fund_name = []
-    list_fund_net_value = []
-    list_fund_cum_net_value = []
+    list_id = []
+    list_code = []
+    list_name = []
+    list_netvalue = []
+    list_cumnetvalue = []
     for row in rows:
         id = ''
         code = ''
         name = ''
-        net_value = ''
-        cum_net_value = ''
+        netvalue = ''
+        cumnetvalue = ''
         soup_id = row.find("td", {"class": "xh"})
         soup_code = row.find("td", {"class": "bzdm"})
-        soup_net_value = row.find("td", {"class": "dwjz black"})
-        soup_cum_net_value = row.find("td", {"class": "ljjz black"})
+        soup_netvalue = row.find("td", {"class": "dwjz black"})
+        soup_cumnetvalue = row.find("td", {"class": "ljjz black"})
         if soup_id is not None:
             id = soup_id.string
         if soup_code is not None:
             code = soup_code.string
-        soup_name = row.find("a", {"href": "%s.html"%code})
+        soup_name = row.find("a", {"href": "http://fund.eastmoney.com/%s.html"%code})
         if soup_name is not None:
             name = soup_name.string
-        if soup_net_value is not None:
-            net_value = soup_net_value.string
-        if soup_cum_net_value is not None:
-            cum_net_value = soup_cum_net_value.string
+        if soup_netvalue is not None:
+            netvalue = soup_netvalue.string
+        if soup_cumnetvalue is not None:
+            cumnetvalue = soup_cumnetvalue.string
         if id and code and name and net_value and cum_net_value:  
-            list_fund_id.append(id)
-            list_fund_code.append(code)
-            list_fund_name.append(name)
-            list_fund_net_value.append(net_value)
-            list_fund_cum_net_value.append(cum_net_value)
-    print('len(rows)=%s, len(list_fund_id)=%s, len(fund_code)=%s, len(name)=%s, len(net_value)=%s, len(cum_net_value)=%s'%(len(rows),len(list_fund_id),len(list_fund_code),len(list_fund_name),len(list_fund_net_value),len(list_fund_cum_net_value)))
-    print('fund_id: %s'%list_fund_id)
-    print('fund_code: %s'%list_fund_code)
-    print('list_fund_name: %s'%list_fund_name)
-    print('list_fund_net_value: %s'%list_fund_net_value)
-    print('list_fund_cum_net_value: %s'%list_fund_cum_net_value)
-    print(list_fund_id[197],list_fund_code[197],list_fund_name[197],list_fund_net_value[197],list_fund_cum_net_value[197])
-        
+            list_id.append(id)
+            list_code.append(code)
+            list_name.append(name)
+            list_netvalue.append(netvalue)
+            list_cumnetvalue.append(cumnetvalue)
+    df = pd.DataFrame({ 'code' : list_code,
+                        'name' : list_name,
+                        'netvalue' : list_netvalue,
+                        'cum_netvalue' : list_cumnetvalue })
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLITE_DATABASE_URI = os.path.join(basedir, '../stock.sqlite')
+    conn = sqlite3.connect(SQLITE_DATABASE_URI)
+    df.to_sql('fund_basics', conn, flavor='sqlite', if_exists='replace')
+    if conn:
+        conn.close()
+    print('完成fund_basics表格的更新。')
             
 if __name__ == '__main__':
     # basedir = os.path.abspath(os.path.dirname(__file__))
     # SQLITE_DATABASE_URI = os.path.join(basedir, '../stock.sqlite')
     # print(SQLITE_DATABASE_URI)
-    # try:
-        # conn = sqlite3.connect(SQLITE_DATABASE_URI)
-        # cur = conn.cursor()
-        # update_stock_data(conn)
-    # finally:
-        # if conn:
-            # cur.close()
-            # conn.close()
-    get_fund_lists()        
+    # conn = sqlite3.connect(SQLITE_DATABASE_URI)
+    # cur = conn.cursor()
+    update_stock_data()
+    get_fund_lists()
+    update_fund_data()
