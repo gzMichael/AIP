d101bf3147a3906aefec1897d56ce09f518eb9c6
gzMichael <1960809@qq.com>
2017-03-26 22:41:14 +0800

    fix bug in backtest.py

--- app/backtest.py
+++ app/backtest.py
@@ -32,113 +32,114 @@ def backtest(testtype, code, start, end, period, fund):
     cur = conn.cursor()
     if testtype == '股票':
         table_name = 'stock_%s'%code
-    if testtype == '基金':
+    else:
         table_name = 'fund_%s'%code
-    list_date = []
-    list_asset = []
-    list_cash = []
-    list_holding = []
-    list_price = []
-    list_totalinvestment = []
-    asset = 0
-    cash = 0
-    holding = 0
-    price = 0
-    totalinvestment = 0
-    dt_start = datetime.datetime.strptime(start, "%Y-%m-%d")
-    dt_end = datetime.datetime.strptime(end, "%Y-%m-%d")
-    #定期增加现金的日期与月份
-    day_invest = dt_start.day
-    month_invest = dt_start.month
-    #输入的初始日期和结束日期正常
-    if dt_start <= dt_end:
-        dt = dt_start
-        if testtype == '股票':
-            sql_query = ("SELECT date,close FROM %s WHERE date>='%s' AND date<='%s' "
-                        "ORDER BY date"%(table_name,start,end)
-                        )
-        if testtype == '基金':
-            sql_query = ("SELECT date,cum_netvalue FROM %s WHERE date>='%s' AND date<='%s' "
-                        "ORDER BY date"%(table_name,start,end)
-                        )
-        index = 0
-        result = []
-        try:
-            result = cur.execute(sql_query).fetchall()
-        except sqlite3.OperationalError as error:
-            print('操作Sqlite3数据库出现错误，错误信息是：%s'%error)
-        finally:
-            if conn:
-                conn.close()
-        #print('sql=%s, result=%s, len(result)=%s'%(sql_query, result, len(result)))
-        #查询到历史数据
-        if len(result) > 0:
-            #开始回测过程
-            while dt <= dt_end:
-                #提取当天日期
-                day = dt.day
-                month = dt.month
-                if day == day_invest:
-                    if period == 'monthly':
-                        cash = cash + fund
-                        asset = asset + fund
-                        totalinvestment = totalinvestment + fund
-                        isnotinvested = True    
-                    else:
-                        if month == month_invest:
+    sql_str = "SELECT name FROM sqlite_master WHERE type='table' AND name='%s'"%table_name
+    rs = []
+    try:
+        rs = conn.execute(sql_str)
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+    if len(rs) > 0:
+        list_date = []
+        list_asset = []
+        list_cash = []
+        list_holding = []
+        list_price = []
+        list_totalinvestment = []
+        asset = 0
+        cash = 0
+        holding = 0
+        price = 0
+        totalinvestment = 0
+        dt_start = datetime.datetime.strptime(start, "%Y-%m-%d")
+        dt_end = datetime.datetime.strptime(end, "%Y-%m-%d")
+        #定期增加现金的日期与月份
+        day_invest = dt_start.day
+        month_invest = dt_start.month
+        #输入的初始日期和结束日期正常
+        if dt_start <= dt_end:
+            dt = dt_start
+            if testtype == '股票':
+                sql_query = ("SELECT date,close FROM %s WHERE date>='%s' AND date<='%s' "
+                            "ORDER BY date"%(table_name,start,end)
+                            )
+            if testtype == '基金':
+                sql_query = ("SELECT date,cum_netvalue FROM %s WHERE date>='%s' AND date<='%s' "
+                            "ORDER BY date"%(table_name,start,end)
+                            )
+            index = 0
+            result = []
+            try:
+                conn = sqlite3.connect(SQLITE_DATABASE_URI)
+                cur = conn.cursor()
+                result = cur.execute(sql_query).fetchall()
+            except sqlite3.OperationalError as error:
+                print('操作Sqlite3数据库出现错误，错误信息是：%s'%error)
+            finally:
+                if conn:
+                    conn.close()
+            #print('sql=%s, result=%s, len(result)=%s'%(sql_query, result, len(result)))
+            #查询到历史数据
+            if len(result) > 0:
+                #开始回测过程
+                while dt <= dt_end:
+                    #提取当天日期
+                    day = dt.day
+                    month = dt.month
+                    if day == day_invest:
+                        if period == 'monthly':
                             cash = cash + fund
                             asset = asset + fund
                             totalinvestment = totalinvestment + fund
-                            isnotinvested = True
-                    needs_recorded = True
-                record = result[index]
-                date_record = record[0]
-                dt_record = datetime.datetime.strptime(date_record,"%Y-%m-%d")
-                close_record = float(record[1])
-                #当天可交易，判断是否能买入
-                if dt == dt_record:
-                    #余额充足，并且入金后未购买
-                    if cash >= close_record * MIN_BUY_AMOUNT and isnotinvested:
-                        buyamount = floor(cash / (close_record * MIN_BUY_AMOUNT))
-                        holding = holding + buyamount * MIN_BUY_AMOUNT
-                        cash = cash - buyamount * close_record * MIN_BUY_AMOUNT
-                        asset = cash + holding * close_record
-                        #needs_recorded = True
-                        isnotinvested = False
-                    #记录指针下移一条
-                    if index < len(result) - 1:
-                        index = index + 1
-                #账户当天有资金出入或有买入        
-                if needs_recorded:
-                    list_totalinvestment.append(totalinvestment)
-                    list_holding.append(holding)
-                    list_cash.append(cash)
-                    list_price.append(close_record)
-                    list_asset.append(asset)
-                    list_date.append(dt.strftime("%y-%m"))
-                    #print('%s 账户有操作！：asset=%s, cash=%s, holding=%s, price=%s'%(dt.strftime("%Y-%m-%d"),asset,cash,holding,close_record))
-                #模拟往后一天
-                dt = dt + datetime.timedelta(days=1)
-                needs_recorded = False
-        #该品种没有查询到历史行情，只能定期存入现金
-        else:
-            while dt <= dt_end:
-                day = dt.day
-                month = dt.month
-                if day == day_invest:
-                    if period == 'monthly':
-                        cash = cash + fund
-                        asset = cash
-                        totalinvestment = totalinvestment + fund
-                        list_holding.append(0)
+                            isnotinvested = True    
+                        else:
+                            if month == month_invest:
+                                cash = cash + fund
+                                asset = asset + fund
+                                totalinvestment = totalinvestment + fund
+                                isnotinvested = True
+                        needs_recorded = True
+                    record = result[index]
+                    date_record = record[0]
+                    dt_record = datetime.datetime.strptime(date_record,"%Y-%m-%d")
+                    close_record = float(record[1])
+                    #当天可交易，判断是否能买入
+                    if dt == dt_record:
+                        #余额充足，并且入金后未购买
+                        if cash >= close_record * MIN_BUY_AMOUNT and isnotinvested:
+                            buyamount = floor(cash / (close_record * MIN_BUY_AMOUNT))
+                            holding = holding + buyamount * MIN_BUY_AMOUNT
+                            cash = cash - buyamount * close_record * MIN_BUY_AMOUNT
+                            asset = cash + holding * close_record
+                            #needs_recorded = True
+                            isnotinvested = False
+                        #记录指针下移一条
+                        if index < len(result) - 1:
+                            index = index + 1
+                    #账户当天有资金出入或有买入        
+                    if needs_recorded:
+                        list_totalinvestment.append(totalinvestment)
+                        list_holding.append(holding)
                         list_cash.append(cash)
-                        list_price.append(0)
+                        list_price.append(close_record)
                         list_asset.append(asset)
                         list_date.append(dt.strftime("%y-%m"))
-                        list_totalinvestment.append(totalinvestment)
-                    else:
-                        if month == month_invest:
-                            cash = cash + fund    
+                        #print('%s 账户有操作！：asset=%s, cash=%s, holding=%s, price=%s'%(dt.strftime("%Y-%m-%d"),asset,cash,holding,close_record))
+                    #模拟往后一天
+                    dt = dt + datetime.timedelta(days=1)
+                    needs_recorded = False
+            #该品种没有查询到历史行情，只能定期存入现金
+            else:
+                while dt <= dt_end:
+                    day = dt.day
+                    month = dt.month
+                    if day == day_invest:
+                        if period == 'monthly':
+                            cash = cash + fund
                             asset = cash
                             totalinvestment = totalinvestment + fund
                             list_holding.append(0)
@@ -147,96 +148,113 @@ def backtest(testtype, code, start, end, period, fund):
                             list_asset.append(asset)
                             list_date.append(dt.strftime("%y-%m"))
                             list_totalinvestment.append(totalinvestment)
-                dt = dt + datetime.timedelta(days=1)
-        #开始计算汇总测试结果
-        if testtype == '股票':
-            tname = 'stock_basics'
-        if testtype == '基金':
-            tname = 'fund_basics'
-        conn = sqlite3.connect(SQLITE_DATABASE_URI)
-        sql_str = "SELECT name FROM %s WHERE code='%s'"%(tname,code)
-        stockname = conn.execute(sql_str).fetchone()
-        name = ''
-        if stockname:
-            name = stockname[0]
-        PnL = list_asset[len(list_asset)-1] - list_totalinvestment[len(list_totalinvestment)-1]
-        str_PnL = str("%2.f"%PnL)
-        PnL_percent = PnL / list_totalinvestment[len(list_totalinvestment)-1]
-        str_PnL_percent = str("%1.f"%(PnL_percent*100)) + '%'
-        yearcount = dt_end.year - dt_start.year + 1
-        monthcount = (yearcount-1) * 12 + dt_end.month - dt_start.month + 1
-        daycount = (dt_end - dt_start).days
-        if PnL > 0:
-            PnL_year = (pow(PnL_percent,1/yearcount) - 1) * 100
-            PnL_month = (pow(PnL_percent,1/monthcount) - 1) * 100
-            PnL_day = (pow(PnL_percent,1/daycount) - 1) * 100
-        elif PnL < 0:
-            PnL_year = (pow(fabs(PnL_percent),1/yearcount) - 1) * (-100)
-            PnL_month = (pow(fabs(PnL_percent),1/monthcount) - 1) * (-100)
-            PnL_day = (pow(fabs(PnL_percent),1/daycount) - 1) * (-100)
+                        else:
+                            if month == month_invest:
+                                cash = cash + fund    
+                                asset = cash
+                                totalinvestment = totalinvestment + fund
+                                list_holding.append(0)
+                                list_cash.append(cash)
+                                list_price.append(0)
+                                list_asset.append(asset)
+                                list_date.append(dt.strftime("%y-%m"))
+                                list_totalinvestment.append(totalinvestment)
+                    dt = dt + datetime.timedelta(days=1)
+            #开始计算汇总测试结果
+            if testtype == '股票':
+                tname = 'stock_basics'
+            if testtype == '基金':
+                tname = 'fund_basics'
+            conn = sqlite3.connect(SQLITE_DATABASE_URI)
+            sql_str = "SELECT name FROM %s WHERE code='%s'"%(tname,code)
+            stockname = conn.execute(sql_str).fetchone()
+            name = ''
+            if stockname:
+                name = stockname[0]
+            PnL = list_asset[len(list_asset)-1] - list_totalinvestment[len(list_totalinvestment)-1]
+            str_PnL = str("%2.f"%PnL)
+            PnL_percent = PnL / list_totalinvestment[len(list_totalinvestment)-1]
+            str_PnL_percent = str("%1.f"%(PnL_percent*100)) + '%'
+            yearcount = dt_end.year - dt_start.year + 1
+            monthcount = (yearcount-1) * 12 + dt_end.month - dt_start.month + 1
+            daycount = (dt_end - dt_start).days
+            if PnL > 0:
+                PnL_year = (pow(PnL_percent,1/yearcount) - 1) * 100
+                PnL_month = (pow(PnL_percent,1/monthcount) - 1) * 100
+                PnL_day = (pow(PnL_percent,1/daycount) - 1) * 100
+            elif PnL < 0:
+                PnL_year = (pow(fabs(PnL_percent),1/yearcount) - 1) * (-100)
+                PnL_month = (pow(fabs(PnL_percent),1/monthcount) - 1) * (-100)
+                PnL_day = (pow(fabs(PnL_percent),1/daycount) - 1) * (-100)
+            else:
+                PnL_year = 0
+                PnL_month = 0
+                PnL_day = 0
+            str_PnL_year = str("%.1f"%PnL_year) + '%'
+            str_PnL_month = str("%.2f"%PnL_month) + '%'
+            str_PnL_day = str("%.4f"%PnL_day) + '%'
+            #汇总报告数据列表
+            summary = []
+            # summary[0]
+            unit_data = ['测试开始时间', start]
+            summary.append(unit_data)
+            unit_data = ['测试结束时间', end]
+            summary.append(unit_data)
+            unit_data = ['代码类型', testtype]
+            summary.append(unit_data)
+            unit_data = ['品种代码', code]
+            summary.append(unit_data)
+            unit_data = ['测试品种名称', name] 
+            summary.append(unit_data)
+            if period == 'monthly':
+                str_period = '每月'
+                str_investtimes = monthcount
+            else:
+                str_period = '每年'
+                str_investtimes = monthcount // 12
+            unit_data = ['定投周期', str_period]
+            summary.append(unit_data)
+            unit_data = ['每周期定投金额', str('%.1f'%fund)]
+            summary.append(unit_data)
+            unit_data = ['投入期数', str_investtimes]
+            summary.append(unit_data)
+            unit_data = ['累计投入金额', totalinvestment]
+            summary.append(unit_data)
+            unit_data = ['期末账户金额', str('%.2f'%asset)]
+            summary.append(unit_data)
+            # summary[10]
+            unit_data = ['盈亏金额', str_PnL]
+            summary.append(unit_data)
+            unit_data = ['盈亏百分比', str_PnL_percent]
+            summary.append(unit_data)
+            unit_data = ['测试时长(年)', yearcount]
+            summary.append(unit_data)
+            unit_data = ['年化收益率(复合)', str_PnL_year]
+            summary.append(unit_data)
+            unit_data = ['测试时长(月)', monthcount]
+            summary.append(unit_data)
+            unit_data = ['月化收益率(复合)', str_PnL_month]
+            summary.append(unit_data)
+            unit_data = ['测试时长(日)', daycount]
+            summary.append(unit_data)
+            unit_data = ['日化收益率(复合)', str_PnL_day]
+            summary.append(unit_data)
+            btrecords = ([summary, list_date, list_asset, list_cash, 
+                        list_holding, list_price, list_totalinvestment])
+            error_str = ''
+            rscode = 0
+            return btrecords, error_str, rscode
+        #初始日期大于结束日期
         else:
-            PnL_year = 0
-            PnL_month = 0
-            PnL_day = 0
-        str_PnL_year = str("%.1f"%PnL_year) + '%'
-        str_PnL_month = str("%.2f"%PnL_month) + '%'
-        str_PnL_day = str("%.4f"%PnL_day) + '%'
-        #汇总报告数据列表
-        summary = []
-        # summary[0]
-        unit_data = ['测试开始时间', start]
-        summary.append(unit_data)
-        unit_data = ['测试结束时间', end]
-        summary.append(unit_data)
-        unit_data = ['代码类型', testtype]
-        summary.append(unit_data)
-        unit_data = ['品种代码', code]
-        summary.append(unit_data)
-        unit_data = ['测试品种名称', name] 
-        summary.append(unit_data)
-        if period == 'monthly':
-            str_period = '每月'
-            str_investtimes = monthcount
-        else:
-            str_period = '每年'
-            str_investtimes = monthcount // 12
-        unit_data = ['定投周期', str_period]
-        summary.append(unit_data)
-        unit_data = ['每周期定投金额', str('%.1f'%fund)]
-        summary.append(unit_data)
-        unit_data = ['投入期数', str_investtimes]
-        summary.append(unit_data)
-        unit_data = ['累计投入金额', totalinvestment]
-        summary.append(unit_data)
-        unit_data = ['期末账户金额', asset]
-        summary.append(unit_data)
-        # summary[10]
-        unit_data = ['盈亏金额', str_PnL]
-        summary.append(unit_data)
-        unit_data = ['盈亏百分比', str_PnL_percent]
-        summary.append(unit_data)
-        unit_data = ['测试时长(年)', yearcount]
-        summary.append(unit_data)
-        unit_data = ['年化收益率(复合)', str_PnL_year]
-        summary.append(unit_data)
-        unit_data = ['测试时长(月)', monthcount]
-        summary.append(unit_data)
-        unit_data = ['月化收益率(复合)', str_PnL_month]
-        summary.append(unit_data)
-        unit_data = ['测试时长(日)', daycount]
-        summary.append(unit_data)
-        unit_data = ['日化收益率(复合)', str_PnL_day]
-        summary.append(unit_data)
-        btrecords = ([summary, list_date, list_asset, list_cash, 
-                    list_holding, list_price, list_totalinvestment])
-        error_str = ''
-        rscode = 0
-        return btrecords, error_str, rscode
-    #初始日期大于结束日期
+            btrecords = []
+            error_str = '初始日期大于结束日期'
+            rscode = 1
+            return btrecords, error_str, rscode
     else:
         btrecords = []
-        error_str = '初始日期大于结束日期'
-        rscode = 1
+        error_str = '''没有查询到该品种的数据记录，请确认输入的代码/品种是否正确
+                    如果这是数据采集的问题，请与开发者(gzMichael:1960809@qq.com)联系，谢谢！'''
+        rscode = 2
         return btrecords, error_str, rscode
     
 def backtest_chart(btrecords):
